8c79f2b59399b2928cf2b8d3e71161c0cffc2011
Tuomas Hietanen <thorium@iki.fi>
2023-07-12 13:20:19 +0100

    IPv6 -> IPv4 conversion.

--- CloudIP.fs
+++ CloudIP.fs
@@ -12,14 +12,33 @@ module GetIPList =
         reader.ReadToEnd()
 
 // http://fssnip.net/8K/title/ipv4-conversion-snippet
-module IP_Parsing =    
+module IP_Parsing =
   open System
   open System.Net
 
-  let intOfIp (s : string) = 
-    IPAddress.Parse(s.Trim()).GetAddressBytes() 
-    |> Array.rev
-    |> fun e -> BitConverter.ToUInt32 (e, 0)
+  type IPvNetwrok =
+  | IPv4
+ // No native support yet:
+// | IPv6 of uint128
+  
+  let intOfIp (s : string) =
+    let parsed = IPAddress.Parse(s.Trim())
+
+    match parsed.AddressFamily with
+    | System.Net.Sockets.AddressFamily.InterNetworkV6 ->
+        // IPv6 is 128bit. For now, we just map it to IPv4.
+        let convertedIpV4 = parsed.MapToIPv4()
+        let i =
+            convertedIpV4.GetAddressBytes()
+            |> Array.rev
+            |> fun e -> BitConverter.ToUInt32 (e, 0)
+        IPv4, i
+    | _ (* System.Net.Sockets.AddressFamily.InterNetwork *) ->
+        let i = 
+            parsed.GetAddressBytes()
+            |> Array.rev
+            |> fun e -> BitConverter.ToUInt32 (e, 0)
+        IPv4, i 
     
   let ipOfInt (d : uint32) = 
     BitConverter.GetBytes d
@@ -34,11 +53,11 @@ module IP_Parsing =
     [| for i in start .. finish -> ipOfInt i |]
 
   let ipsOfRange (d : string) = 
-    
     let elem = slice d [|"to"; "-"; "and"|]
     let start,finish = intOfIp elem.[0], intOfIp elem.[1]
-    
-    ipArrayOfIntRange start finish
+    match intOfIp elem.[0], intOfIp elem.[1] with
+    | (ipvType1, start), (ipvType2, finish) when ipvType1 = ipvType2 -> ipvType1, ipArrayOfIntRange start finish
+    | _ -> IPv4, Array.empty // no mixed networks
     
   (* "192.168.1.1/24" -> ["192.168.1.1 .. 192.168.1.254"] *)
   let ipsOfCidrs (d : string) =
@@ -47,12 +66,14 @@ module IP_Parsing =
     let lsn x = (1 <<< 32 - x) - 1 |> (~~~)    |> uint32
     let cidr  = Array.get elem 1   |> int
     let mask  = cidr |> int        |> lsn
-    let addr  = elem |> Seq.head   |> intOfIp  |> (&&&) mask
+    let ipvn, ipOfInt = elem |> Seq.head |> intOfIp
+    let addr  = ipOfInt |> (&&&) mask
     let start,finish = addr + 1u, addr + ~~~mask - 1u
 
-    if cidr > 30 then [| elem |> Seq.head |]
-    else
-      ipArrayOfIntRange start finish
+    let limit = match ipvn with | IPv4 -> 30 //| _ -> 60
+
+    if cidr > limit then ipvn, [| elem |> Seq.head |]
+    else ipvn, ipArrayOfIntRange start finish
 
 
   // Some additions, making large arrays would be too slow
@@ -63,19 +84,19 @@ module IP_Parsing =
     let lsn x = (1 <<< 32 - x) - 1 |> (~~~)    |> uint32
     let cidr  = Array.get elem 1   |> int
     let mask  = cidr |> int        |> lsn
-    let addr  = elem |> Seq.head   |> intOfIp  |> (&&&) mask
+    let ipvn, ipOfInt = elem |> Seq.head |> intOfIp
+    let addr  = ipOfInt |> (&&&) mask
     let start,finish = addr + 1u, addr + ~~~mask - 1u
+    let limit = match ipvn with | IPv4 -> 30 //| _ -> 60
 
-    if cidr > 30 then 
-      addr, addr
-    else
-      start, finish
+    if cidr > limit then ipvn, addr, addr
+    else ipvn, start, finish
 
-  let isWithinRange ip (ranges:(uint32*uint32)[]) =
-    let i = intOfIp ip
-    ranges |> Array.exists(fun (start, finish) -> i >= start && i <= finish)
+  let isWithinRange (checkNtwrk:IPvNetwrok, ipAsInt:uint32) (ranges:(IPvNetwrok*uint32*uint32)[]) =
+    ranges |> Array.exists(fun (nwrk, start, finish) -> nwrk = checkNtwrk && ipAsInt >= start && ipAsInt <= finish)
 
 type SupportedCloudServices =
+| InvalidIp
 | LocalIp
 | CloudFlare
 | Fastly
@@ -110,14 +131,15 @@ module Cloudservices =
     let fastlyCDN = 
         let ranges = FastlyData.Load "https://api.fastly.com/public-ip-list"
         let ipv4s = ranges.Addresses |> Array.map IP_Parsing.uintsOfCidrs
-        ipv4s
+        //let ipv6s = ranges.Ipv6Addresses |> Array.map IP_Parsing.uintsOfCidrs
+        ipv4s //Array.concat [ ipv4s ; ipv6s]
 
     let cloudFlares =
-        let ipsFile = Uri "https://www.cloudflare.com/ips-v4"
-        let data = GetIPList.fetch ipsFile
-        let ip_list = data.Replace("\r", "").Split('\n') |> Array.filter(String.IsNullOrEmpty >> not)
-        ip_list 
-        |> Array.filter(fun ipv4 -> ipv4.Contains(".")) // For now, IPv4 only
+        let data4 = "https://www.cloudflare.com/ips-v4" |> Uri |> GetIPList.fetch
+        let ip_list4 = data4.Replace("\r", "").Split('\n') |> Array.filter(String.IsNullOrEmpty >> not)
+        //let data6 = "https://www.cloudflare.com/ips-v6" |> Uri |> GetIPList.fetch
+        //let ip_list6 = data6.Replace("\r", "").Split('\n') |> Array.filter(String.IsNullOrEmpty >> not)
+        ip_list4 //Array.concat [ ip_list4 ; ip_list6 ]
         |> Array.map IP_Parsing.uintsOfCidrs
 
     let aws =
@@ -145,22 +167,36 @@ module Cloudservices =
         ipv4s
 
     let checkIp(ip:string) =
-        if String.IsNullOrWhiteSpace ip || ip.Length < 3 || ip.Length > 15 || not(ip.Contains ".") then
+        if String.IsNullOrWhiteSpace ip || ip.Length < 3 || ip.Length > 46 || not(ip.Contains "." || ip.Contains ":") then
+            // Not an ip
+            false, Some InvalidIp
+        else
+        if ip = "::1" || ip = "127.0.0.1" then
             // Not supported IP
-            false, None
+            true, Some LocalIp
         else
 
-        if (localIps |> IP_Parsing.isWithinRange ip) || ip = "::1" then
-            true, Some LocalIp
-        elif cloudFlares |> IP_Parsing.isWithinRange ip then
-            true, Some CloudFlare
-        elif fastlyCDN |> IP_Parsing.isWithinRange ip then
-            true, Some Fastly
-        elif aws |> IP_Parsing.isWithinRange ip then
-            true, Some AWS
-        elif gitHub |> IP_Parsing.isWithinRange ip then
-            true, Some GitHub
-        else false, None
+        let ipInt =
+            try
+                Some (IP_Parsing.intOfIp ip)
+            with
+            | :? FormatException -> None
+
+        match ipInt with
+        | None -> true, Some InvalidIp
+        | Some uIp ->
+
+            if (localIps |> IP_Parsing.isWithinRange uIp) then
+                true, Some LocalIp
+            elif cloudFlares |> IP_Parsing.isWithinRange uIp then
+                true, Some CloudFlare
+            elif fastlyCDN |> IP_Parsing.isWithinRange uIp then
+                true, Some Fastly
+            elif aws |> IP_Parsing.isWithinRange uIp then
+                true, Some AWS
+            elif gitHub |> IP_Parsing.isWithinRange uIp then
+                true, Some GitHub
+            else false, None
 
     let checkIps(ips:string[]) =
         let res = ips |> Array.map(fun i -> i, checkIp i)
